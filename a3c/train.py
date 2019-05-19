d1ccfa4ed9e8c553cabdde182780eea8fea9ba17
mfsuve <suvemustafa@gmail.com>
2019-05-19 12:30:36 +0300

    Created Worker.run function, Started Worker Processes

--- a3c/train.py
+++ a3c/train.py
@@ -1,41 +1,58 @@
-from gym_torcs import TorcsEnv
-from mp_utils import Counter
-import torch.multiprocessing as mp
+from a3c_utils import Counter, A3C_args, TrajectoryList
+from torch.multiprocessing import Lock, Process
 import torch
 import numpy as np
+from network import A3C_Network
+from agent import A3C_Agent
+from shared_adam import Shared_Adam
 
-class Worker(mp.Process):
-    def __init__(self, rank, global_net, counter, max_time):
+class Worker(Process):
+    def __init__(self, rank, global_net, counter, lock, opt, args):
         super(Worker, self).__init__()
+        self.agent = A3C_Agent(rank, global_net, lock, opt, args)
         self.rank = rank
-        self.global_net = global_net
         self.counter = counter
-        self.max_time = max_time
+        self.args = args
 
     def run(self):
         eps_n = 0
-        while self.counter.value() < self.max_time:
-            pass
+        eps_time = 0
+        self.agent.reset()
+        while self.counter.value() < self.args.max_time:
+
+            trajectories = TrajectoryList()
+            for step in range(self.args.nstep):
+                action, value, log_prob, entropy = self.agent.soft_policy()
+                self.agent.state, reward, done, info = self.agent.env.step(action)
+                # reward = max(min(float(reward), 1.0), -1.0)
+
+                eps_time += 1
+                self.agent.done = done or eps_time >= args.max_eps_time
+                self.counter.increment()
+
+                trajectories.append(value, reward, log_prob, entropy)
+
+                if self.agent.done:
+                    break
+
+            if self.agent.done:
+                eps_time = 0
+                eps_n += 1
+
+            self.agent.update(trajectories)
+            self.agent.reset()
+
+            # break
 
 
+counter = Counter()
+lock = Lock()
+args = A3C_args()
+global_net = A3C_Network(29, 3)
+global_net.share_memory()
+opt = Shared_Adam(global_net.parameters(), lr=args.lr)
 
-env = TorcsEnv(path='/usr/local/share/games/torcs/config/raceman/quickrace.xml')
+workers = [Worker(i, global_net, counter, lock, opt, args) for i in range(args.num_workers)]
 
-state = env.reset(relaunch=True, sampletrack=True, render=False)
-print('State:', state)
-print('Angle        :', state[0])
-print('Track        :', state[1:20])
-print('TrackPos     :', state[20])
-print('Speed        :', state[21:24])
-print('WheelSpeeds  :', state[24:28])
-print('rpm          :', state[28])
-eps_reward = 0
-eps_n = 1
-for i in range(500):
-    state, reward, done, _ = env.step([0., 0., 0.])
-    eps_reward += reward
-    if done:
-        print('Episode', eps_n, 'Reward:', eps_reward)
-        state = env.reset(relaunch=eps_n%5==0, sampletrack=True, render=False)
-        eps_reward = 0
-        eps_n += 1
+for w in workers: w.start()
+for w in workers: w.join()
