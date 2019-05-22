c1821c15c8392a4e0b9144c6b46033ae66d49776
mfsuve <suvemustafa@gmail.com>
2019-05-22 11:49:35 +0300

    Creating and starting testing process starting processes without sleeping

--- a3c/main.py
+++ a3c/main.py
@@ -1,9 +1,9 @@
-from a3c_utils import Counter, A3C_args
-from torch.multiprocessing import Lock
+from a3c_utils import Counter, A3C_args, Recorder
+from torch.multiprocessing import Lock, Manager
 import torch
 import numpy as np
 from network import A3C_Network
-from agent import A3C_Agent
+from agent import Worker, Tester
 from shared_adam import Shared_Adam
 from time import sleep
 
@@ -14,13 +14,15 @@ global_net = A3C_Network(29, 3)
 global_net.share_memory()
 opt = Shared_Adam(global_net.parameters(), lr=args.lr)
 
-workers = []
-for i in range(args.num_workers):
-    worker = A3C_Agent(i, global_net, counter, lock, opt, args)
-    workers.append(worker)
+# Creating training processes
+workers = [Worker(i, global_net, counter, lock, opt, args) for i in range(args.num_workers)]
+# Creating testing process
+recorder = Recorder(Manager())
+workers.append(Tester(args.num_workers, global_net, counter, lock, recorder, args))
 
 for w in workers:
     w.start()
-    sleep(0.1)
+    # sleep(0.1)
 
-for w in workers: w.join()
+for w in workers:
+    w.join()
