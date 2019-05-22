9802c32d9fb024bb7617ca65a531eaf618bb8acf
mfsuve <suvemustafa@gmail.com>
2019-05-22 11:50:38 +0300

    Created server for remote displaying logs

---
+++ a3c/logger/log_server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+import time
+
+app = Flask(__name__, template_folder='.')
+
+@app.route('/', methods=['POST'])
+def index():
+    msg = request.form['msg']
+    with open('logs.txt', 'a+') as log_file:
+        time_str = time.strftime('%d-%b-%y %H:%M:%S', time.localtime())
+        print(f'{time_str}\t{msg}', file=log_file)
+        print(f'{time_str}\t{msg}')
+    return ""
+
+@app.route('/log')
+def log():
+    try:
+        with open('logs.txt', 'r+') as text:
+            content = text.read()
+    except FileNotFoundError:
+        content = 'No logs found.'
+    return render_template('content.html', text=content)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=3000, debug=True)
