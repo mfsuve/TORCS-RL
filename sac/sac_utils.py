a37d87e3b0a7c1f095529df8f739ae7e97635566
mfsuve <suvemustafa@gmail.com>
2019-05-25 23:57:31 +0300

    OrnsteinUhlenbeckProcess implemented, Updated args, added clipgrads and sigma

--- sac/sac_utils.py
+++ sac/sac_utils.py
@@ -5,22 +5,44 @@ import time
 class SAC_args:
     def __init__(self):
         self.max_eps = 100000
-        self.max_eps_time = 500
-        self.gamma = 0.99
-        self.soft_tau = 1e-2
-        self.lr = 3e-4
+        self.max_eps_time = 1000
+        self.gamma = 0.985
+        self.soft_tau = 0.01
+        self.lr = 0.001
         self.alpha = 0.2
-        self.buffer_size = 1000000
+        self.buffer_size = 300000
         self.batch_size = 128
         self.device = 'cpu'
         self.test_rate = 5
-        self.test_per = 10
+        self.test_per = 50
+        self.clipgrad = True
+        self.start_sigma = 0.9
+        self.end_sigma = 0.1
+
+
+class OrnsteinUhlenbeckProcess(object):
+
+    def __init__(self, theta, sigma, dim, mu=0):
+        self.theta = theta
+        self.sigma = sigma
+        self.mu = mu
+        self.state = mu
+        self.dim = dim
+
+    def noise(self):
+        v = self.theta * (self.mu - self.state) + self.sigma * torch.randn(self.dim)
+        self.state += v
+        return self.state
+
+    def reset(self):
+        self.state = self.mu
 
 
 def log(msg):
     with open('logger/logs.txt', 'a+') as log_file:
         time_str = time.strftime('%d-%b-%y %H:%M:%S', time.localtime())
         print(f'{time_str}\t{msg}', file=log_file)
+
 
 def remove_log_file():
     if os.path.exists('logger/logs.txt'):
