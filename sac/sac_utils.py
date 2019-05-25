7574e8efa2bdd3714f661cb2b29d11773088c65b
mfsuve <suvemustafa@gmail.com>
2019-05-26 01:01:21 +0300

    Checkpoint class implemented

--- sac/sac_utils.py
+++ sac/sac_utils.py
@@ -1,5 +1,6 @@
 import os
 import time
+import torch
 
 
 class SAC_args:
@@ -37,6 +38,30 @@ class OrnsteinUhlenbeckProcess(object):
 
     def reset(self):
         self.state = self.mu
+
+
+class Checkpoint(object):
+    def __init__(self, folder=None):
+        self.state = {
+            'best_value': None,
+            'best_q1': None,
+            'best_q2': None,
+            'best_policy': None
+        }
+        if folder is None:
+            self.folder = '.'
+        else:
+            self.folder = folder
+
+    def save(self, filename):
+        path = os.path.join(self.folder, filename)
+        torch.save(self.state, path)
+
+    def update(self, best_value, best_q1, best_q2, best_policy):
+        self.state['best_value'] = best_value
+        self.state['best_q1'] = best_q1
+        self.state['best_q2'] = best_q2
+        self.state['best_policy'] = best_policy
 
 
 def log(msg, end=None):
