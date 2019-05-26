01bcb3693e14549c39573a2fe48e7d8cb4e64cc2
mfsuve <suvemustafa@gmail.com>
2019-05-26 12:29:08 +0300

    Loading checkpoint and racing options added

--- sac/main.py
+++ sac/main.py
@@ -1,4 +1,12 @@
 from agent import SAC_Agent
 
+train = True
+load_from = 'model/26-May-19-04.45.07/e136r13867.955340322758.pth'
+
+# agent = SAC_Agent(load_from)
 agent = SAC_Agent()
-agent.train()
+
+if train:
+    agent.train()
+else:
+    agent.race()
