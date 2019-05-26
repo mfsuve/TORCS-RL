4c49da410f35f4a2609abf8080878a105531cc90
mfsuve <suvemustafa@gmail.com>
2019-05-26 18:29:18 +0300

    Positive reward if place gets better negative if worse

--- sac/gym_torcs.py
+++ sac/gym_torcs.py
@@ -79,6 +79,7 @@ class TorcsEnv:
             np.array([0.0]),
         ])
         self.observation_space = spaces.Box(low=low, high=high)
+        self.place = 7
 
     def step(self, u):
         assert self.initial_reset == False, "Call the reset() function before step() function!"
@@ -165,6 +166,11 @@ class TorcsEnv:
 
         self.time_step += 1
 
+        # giving positive rewards for passing someone
+        # and negative rewards for being passed by someone
+        reward += (self.place - info["place"]) * 2
+        self.place = info["place"]
+
         return self.get_obs(), reward, episode_terminate, info
 
     def reset(self, relaunch=False, sampletrack=False, render=False):
@@ -213,6 +219,8 @@ class TorcsEnv:
 
         obs = client.S.d  # Get the current full-observation from torcs
         self.observation = self.make_observaton(obs)
+
+        self.place = obs["place"]
 
         self.initial_reset = False
         return self.get_obs()
